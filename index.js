54a06f50b5d7c615b26108174d6154e4fa2ee506
Drew Rollins <drewrollins14@gmail.com>
2018-05-12 00:15:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,7 @@ function theBeatlesPlay(players, instruments) {
 }
 
 
-function johnLennonFacts() {
-  var facts = [
-        "He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"
-      ];
+function johnLennonFacts(facts) {
 
     var newArray = [];
     var i = 0;
